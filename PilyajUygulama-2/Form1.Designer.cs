020d954820136e3f471009bc99a3420bd27919e2
Seraceddin <53696758+Seraceddin@users.noreply.github.com>
2020-09-15 23:51:30 +0300

    Add files via upload

---
+++ PilyajUygulama-2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PilyajUygulama_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dosya";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(477, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(667, 7);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(822, 434);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(248, 173);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(822, 34);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(248, 394);
+            this.listBox2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(1, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 600);
+            this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(832, 621);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tahta Ölçüleri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(956, 621);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(822, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Temzile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tual Lazer Pilyaj";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PilyajUygulama-2/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilyajUygulama_2
+{
+    public partial class Form1 : Form
+    {
+        float xxmin,yymin,xxmax,yymax;
+        string xmin,ymin,xmax,ymax;
+        string tahtax, tahtay, klm_reng;
+        float oranx, orany;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            panel1.Enabled = true;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.Text = listBox1.SelectedItem.ToString();
+        }
+
+        string ittem;
+        OpenFileDialog openFileDialog1 = new OpenFileDialog()
+        {
+            CheckFileExists = false,
+            CheckPathExists = false,
+            Multiselect = false,
+            Title = "Yeni İş Aç",
+            DefaultExt = "dxf",
+            Filter = "dxf files (*.dxf)|*.dxf",
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            string dosya = textBox1.Text;
+            string oku;
+            StreamReader str = new StreamReader(dosya);
+            oku = str.ReadLine();
+            while(oku!=null)
+            {
+                listBox2.Items.Add(oku);
+                oku = str.ReadLine();
+            }
+            str.Close();
+            for (int say=0; say<listBox2.Items.Count; say++)
+            {
+                listBox2.SetSelected(say, true); //if kontrollerinde item bilgisini yakalamak için gerekli
+                if (listBox2.SelectedItem.ToString() == "HEADER")
+                {
+                    listBox2.SetSelected(say + 14, true);
+                    tahtax = listBox2.SelectedItem.ToString();
+                    listBox2.SetSelected(say + 16, true);
+                    tahtay = listBox2.SelectedItem.ToString();
+                    label2.Text = tahtax + "X" + tahtay; // burada headerden tahta boyutları alınır
+
+                    oranx = float.Parse(tahtax, CultureInfo.InvariantCulture.NumberFormat) / 785;
+                    orany = float.Parse(tahtay, CultureInfo.InvariantCulture.NumberFormat) / 585;
+                    label1.Text = oranx.ToString() + "-" + orany.ToString();
+                    //oranlar hesaplanır
+                }
+
+                if (listBox2.SelectedItem.ToString() == "LINE") // item "LINE" ise yapılacaklar
+                {
+                    ittem = listBox2.SelectedItem.ToString();
+
+                    listBox2.SetSelected(say + 6, true);                                    
+                    klm_reng = listBox2.SelectedItem.ToString();
+
+                    listBox2.SetSelected(say + 8, true);                                    //
+                    xmin = listBox2.SelectedItem.ToString();                                // minumum x koordinat bilgisi
+                    xxmin =float.Parse(xmin, CultureInfo.InvariantCulture.NumberFormat);    //
+                    listBox2.SetSelected(say + 10, true);                                   // 
+                    ymin = listBox2.SelectedItem.ToString();                                // minumum y koordinat bilgisi
+                    yymin = float.Parse(ymin, CultureInfo.InvariantCulture.NumberFormat);   //
+                    listBox2.SetSelected(say + 12, true);                                   //
+                    xmax = listBox2.SelectedItem.ToString();                                // maximum x koordinat bilgisi
+                    xxmax = float.Parse(xmax, CultureInfo.InvariantCulture.NumberFormat);   //
+                    listBox2.SetSelected(say + 14, true);                                   //
+                    ymax = listBox2.SelectedItem.ToString();                                // maximum y koordinat bilgisi
+                    yymax = float.Parse(ymax, CultureInfo.InvariantCulture.NumberFormat);   //
+                    czz();  // "LINE" bilgilerine göre çiz
+                    if(float.Parse(klm_reng)==2)
+                    {
+                        listBox1.Items.Add(say + " " + ittem + " " + xxmin + " " + yymin + " " + xxmax + " " + yymax);
+                    }
+                }
+                
+            }
+            textBox2.Text = listBox2.Items.Count.ToString(); // Bize dxf de kaç satır olduğu bilgisini verir.
+            
+        }
+        void czz()
+        {
+            Pen klm;
+            Graphics gp = panel1.CreateGraphics();
+            if(float.Parse(klm_reng) == 1)
+            {
+                 klm = new Pen(Color.White, 1);
+            }
+            else if (float.Parse(klm_reng) == 2)
+            {
+                 klm = new Pen(Color.Green, 1);
+            }
+            else
+            {
+                klm = new Pen(Color.Red, 1);
+            }
+            gp.DrawLine(klm, 5 + xxmin, 5 + yymin, 5 + xxmax, 5 + yymax);
+            //gp.DrawLine(klm, 5+(xxmin/oranx), 5+(yymin/orany), 5+(xxmax/oranx), 5+(yymax/orany)); //Burada verilen 5 grafiğin kenardan uzaklığı olarak verilmiştir.
+            // oranx ve oran y cizimin ölçülerine göre panele orantılı yerleştirilmesi için kullanılmıştır.
+        }
+    } 
+}
